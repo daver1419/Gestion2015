781a54ef83f3ee67f60485532cdc63c1dd8bd9db
Romina Cuadra <romicuadra@gmail.com>
2015-05-24 02:08:16 -0300

    Creacion de mas tablas.

--- ScriptMigracion.sql
+++ ScriptMigracion.sql
@@ -1,102 +1,180 @@
+/********************************************* INICIO - CREACION ESQUEMA *******************************************/
+
 USE GD1C2015;
 GO
+/*CREATE SCHEMA THE_ULTIMATES AUTHORIZATION gd
+
+GO */
+
+/********************************************* FIN - CREACION ESQUEMA **********************************************/
+
+/******************************************** INICIO - CREACION DE TABLAS ******************************************/
 
 create table THE_ULTIMATES.Rol( 
-	rol_id tinyint not null,
-	rol_desc char(15) null,
-	rol_activo bit not null);
+	rol_id tinyint PRIMARY KEY NOT NULL IDENTITY(1,1),
+	rol_desc varchar(15),
+	rol_activo bit DEFAULT 1  
+	);
+
+GO	
+
+create table THE_ULTIMATES.Funcionalidad(
+	func_id tinyint PRIMARY KEY NOT NULL IDENTITY(1,1),
+	func_desc varchar(60)
+	);
+
+GO
 	
 create table THE_ULTIMATES.Funcionalidad_Rol(
- func_rol_rol_id smallint not null,
- func_rol_func_id smallint not null);
- 
-create table THE_ULTIMATES.Funcionalidad(
-	func_id tinyint not null,
-	func_desc char(20) null);
-		
-create table THE_ULTIMATES.Rol_Usuario(
-	rol_usua_rol_id smallint not null,
-	rol_usua_usua_id smallint not null);
-	
+	func_rol_id  tinyint PRIMARY KEY NOT NULL IDENTITY(1,1),
+	func_rol_rol_id smallint  not null UNIQUE, /* FK  THE_ULTIMATES.Rol*/
+	func_rol_func_id smallint not null UNIQUE /* FK  THE_ULTIMATES.Funcionalidad*/
+	); 
+
+GO
+
 create table THE_ULTIMATES.Usuario(
-	usua_id int not null,
-	usua_username char(20) not null unique,
-	usua_password char(64) not null,
-	usua_fecha_alta datetime not null,
-	usua_fecha_mod datetime null,
-	usua_pregunta varchar(50) not null,
-	usua_respuesta char(64) not null,
-	usua_activo bit not null,
-	usua_intentos_fallidos tinyint not null);
+	usu_id int PRIMARY KEY NOT NULL IDENTITY(1,1),
+	usu_username varchar(20) not null UNIQUE,
+	usu_password char(64) not null,
+	usu_fecha_alta datetime not null,
+	usu_fecha_mod datetime null,
+	usu_pregunta varchar(50),
+	usu_respuesta char(64),
+	usu_activo bit DEFAULT 0,
+	usu_intentos_fallidos tinyint not null,
+	usu_clie_id int null
+	 /* FK  THE_ULTIMATES.CLiente*/
+	);
+
+GO
+
+create table THE_ULTIMATES.Rol_Usuario(
+	rol_usu int PRIMARY KEY NOT NULL IDENTITY(1,1),
+	rol_usu_rol_id smallint  not null UNIQUE, /* FK  THE_ULTIMATES.Rol*/
+	rol_usu_usu_id smallint  not null UNIQUE  /* FK  THE_ULTIMATES.Usuario*/
+	);
 	
+GO
+
 create table THE_ULTIMATES.Acceso_Log(
-	acce_id int not null,
-	acce_usuario int not null,
-	acce_fecha datetime not null,
-	acce_correcto bit not null,
-	acce_numero_fallido tinyint not null);
+	acc_id int PRIMARY KEY not null IDENTITY(1,1),
+	acc_usu_id int  not null, /* FK  THE_ULTIMATES.Usuario*/
+	acc_fecha datetime not null,
+	acc_correcto bit not null,
+	acc_intent_fallido tinyint not null
+	);
+
+GO
 	
 create table THE_ULTIMATES.Pais(
-	pais_id smallint not null,
-	pais_desc varchar(50) not null);
+	pais_id smallint PRIMARY KEY not null IDENTITY(1,1),
+	pais_desc varchar(50) not null
+	);
+
+GO
 
 create table THE_ULTIMATES.Tipo_Doc(
-	tipo_doc_id smallint not null,
-	tipo_doc_desc char(20) not null);
+	tipo_doc_id smallint PRIMARY KEY not null IDENTITY(1,1),
+	tipo_doc_desc char(20) not null
+	);
+
+GO
 	
 create table THE_ULTIMATES.Cliente(
-	clie_id int not null,
+	clie_id int PRIMARY KEY not null IDENTITY(1,1),
 	clie_nombre varchar(50) not null,
 	clie_apellido varchar(50) not null,
-	clie_nro_doc int not null,
-	clie_tipo_doc smallint not null,
+	clie_nro_doc int not null unique, 
+	clie_tipo_doc smallint not null, /* FK  THE_ULTIMATES.Tipo_Doc*/
 	clie_mail varchar(50) not null unique,
 	clie_activo bit not null,
 	clie_dom_calle varchar(50) not null,
 	clie_dom_numero int not null,
 	clie_dom_piso smallint null,
-	clie_dom_depto varchar(10) null,
+	clie_dom_depto varchar(10),
 	clie_fecha_nac datetime not null,
-	clie_pais smallint not null);
+	clie_pais smallint not null /* FK  THE_ULTIMATES.Pais*/
+	);
+
+GO
 	
 create table THE_ULTIMATES.Cuenta(
-	cuen_id int not null,
-	cuen_cliente int not null,
-	cuen_tipo_cuenta tinyint not null,
+	cuen_id int PRIMARY KEY not null IDENTITY(1,1),
+	cuen_clie_id int not null, /* FK  THE_ULTIMATES.Cliente*/
+	cuen_tipo_cuenta tinyint not null, /* FK  THE_ULTIMATES.Tipo_Cuenta*/
 	cuen_fecha_creacion datetime not null,
-	cuen_fecha_cierre datetime null,
-	cuen_estado tinyint not null,
-	cuen_pais smallint not null,
+	cuen_fecha_cierre datetime,
+	cuen_estado_id tinyint not null, /* FK  THE_ULTIMATES.Estado_Cuenta*/
+	cuen_pais_id smallint not null, /* FK  THE_ULTIMATES.Pais*/
 	cuen_saldo numeric(18,2) not null,
-	cuen_tipo_moneda smallint not null);
+	cuen_tipo_mon_id smallint not null /* FK  THE_ULTIMATES.Tipo_Moneda*/
+	);
+
+GO
 	
 create table THE_ULTIMATES.Tipo_Moneda(
-	tipo_moneda_id smallint not null,
-	tipo_moneda_desc char(15) not null,
-	tipo_moneda_cambio numeric(18,3)not null);
+	tipo_moneda_id smallint PRIMARY KEY not null IDENTITY(1,1),
+	tipo_moneda_desc varchar(30) not null,
+	tipo_moneda_cambio numeric(18,3)not null
+	);
+
+GO
 	
 create table THE_ULTIMATES.Estado_Cuenta(
-	esta_cuenta_id tinyint not null,
-	esta_cuenta_desc char(20) not null);
+	esta_cuenta_id tinyint PRIMARY KEY not null IDENTITY(1,1),
+	esta_cuenta_desc varchar(30) not null
+	);
+
+GO
 
 create table THE_ULTIMATES.Tipo_Cuenta(
-	tipo_cuenta_id tinyint not null,
-	tipo_cuenta_desc char(20)not null,
+	tipo_cuenta_id tinyint PRIMARY KEY not null IDENTITY(1,1),
+	tipo_cuenta_desc varchar(30)not null,
 	tipo_cuenta_duracion smallint not null,
-	tipo_cuenta_costo numeric(18,2) not null);
+	tipo_cuenta_costo numeric(18,2) not null
+	);
+
+GO
 
 create table THE_ULTIMATES.Tarjeta(
-	tarj_id int not null,
-	tarj_cliente int not null,
-	tarj_numero char(64) not null,
+	tarj_id int PRIMARY KEY not null IDENTITY(1,1),
+	tarj_clie_id int not null,  /* FK  THE_ULTIMATES.Cliente*/
+	tarj_numero char(64) not null UNIQUE,
 	tarj_numero_preview char(4) not null,
-	tarj_emisor tinyint not null,
+	tarj_emisor_id tinyint not null, /* FK  THE_ULTIMATES.Emisor*/
 	tarj_fecha_emision datetime not null,
-	tarj_fecha_vencimiento datetime not null,
-	tarj_codigo_seguridad char(4),
-	tarj_activa bit not null);
+	tarj_fecha_venc datetime not null,
+	tarj_codigo_seg char(4),
+	tarj_activa bit not null
+	);
 
+GO
 
+create table THE_ULTIMATES.Emisor(
+	emisor_id int PRIMARY KEY not null IDENTITY(1,1),
+	emisor_desc varchar(30)
+	); 
+	
+GO
+
+create table THE_ULTIMATES.Deposito(
+	depo_id int PRIMARY KEY NOT NULL IDENTITY(1,1),
+	depo_fecha datetime not null,
+	depo_importe numeric(18,2) not null,
+	depo_cuenta_id int not null,  /* FK  THE_ULTIMATES.Cuenta*/
+	depo_tarj_id int not null,  /* FK  THE_ULTIMATES.Tarjeta*/
+	depo_tipo_moneda_id int not null /* FK  THE_ULTIMATES.Tipo_Moneda*/
+	);
+	
+GO
+
+create table THE_ULTIMATE.Cheque(
+	cheque_id int PRIMARY KEY NOT NULL IDENTITY(1,1),
+	cheque_fecha datetime not null,
+	cheque_importe numeric(18,3)not null,
+	cheque_banco_id int not null  /* FK  THE_ULTIMATES.Banco*/
+	);
 	
 	
 	
