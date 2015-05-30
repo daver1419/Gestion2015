64b5eda31510e999b424da3dc7bd02eaae5d9936
David <daver1419@gmail.com>
2015-05-30 01:14:23 -0300

    Agregado de Foreign Keys a todas las tablas

--- DeleteTables.sql
+++ DeleteTables.sql
@@ -1,6 +1,35 @@
 USE GD1C2015
 GO
 
+
+alter table THE_ULTIMATES.Funcionalidad_Rol drop constraint FK_func_ron_rol_id;
+alter table THE_ULTIMATES.Funcionalidad_Rol drop constraint FK_func_ron_func_id;
+alter table THE_ULTIMATES.Usuario drop constraint FK_usu_clie_id;
+alter table THE_ULTIMATES.Rol_Usuario drop constraint FK_rol_usu_rol_id;
+alter table THE_ULTIMATES.Rol_Usuario drop constraint FK_rol_usu_usu_id;
+alter table THE_ULTIMATES.Acceso_Log drop constraint FK_acc_usu_id;
+alter table THE_ULTIMATES.Cliente drop constraint FK_clie_tipo_doc_id;
+alter table THE_ULTIMATES.Cliente drop constraint FK_clie_pais_id;
+alter table THE_ULTIMATES.Cuenta drop constraint FK_cuen_clie_id;
+alter table THE_ULTIMATES.Cuenta drop constraint FK_cuen_tipo_cuenta_id;
+alter table THE_ULTIMATES.Cuenta drop constraint FK_cuen_estado_id;
+alter table THE_ULTIMATES.Cuenta drop constraint FK_cuen_pais_id;
+alter table THE_ULTIMATES.Cuenta drop constraint FK_cuen_tipo_mon_id;
+alter table THE_ULTIMATES.Tarjeta drop constraint FK_tarj_clie_id;
+alter table THE_ULTIMATES.Tarjeta drop constraint FK_tarj_emisor_id;
+alter table THE_ULTIMATES.Deposito drop constraint FK_depo_cuen_id;
+alter table THE_ULTIMATES.Deposito drop constraint FK_depo_tarj_id;
+alter table THE_ULTIMATES.Deposito drop constraint FK_depo_tipo_moneda_id;
+alter table THE_ULTIMATES.Cheque drop constraint FK_cheque_banco_id;
+alter table THE_ULTIMATES.Transferencia drop constraint FK_transf_cuenta_origen;
+alter table THE_ULTIMATES.Transferencia drop constraint FK_transf_cuenta_destino;
+alter table THE_ULTIMATES.Transaccion drop constraint FK_transac_tipo_transac_id;
+alter table THE_ULTIMATES.Extraccion drop constraint FK_extrac_cuenta_id;
+alter table THE_ULTIMATES.Extraccion drop constraint FK_extrac_cheque_id;
+alter table THE_ULTIMATES.Factura drop constraint FK_fact_clie_id;
+alter table THE_ULTIMATES.Item_Factura drop constraint FK_item_fact_num;
+/*alter table THE_ULTIMATES.Item_Factura drop constraint FK_item_fact_transac_id;*/
+
 drop table THE_ULTIMATES.Acceso_Log;
 drop table THE_ULTIMATES.Cliente;
 drop table THE_ULTIMATES.Cuenta;
--- ScriptMigracion.sql
+++ ScriptMigracion.sql
@@ -26,15 +26,15 @@ create table THE_ULTIMATES.Funcionalidad(
 GO
 	
 create table THE_ULTIMATES.Funcionalidad_Rol(
-	func_rol_id  tinyint PRIMARY KEY NOT NULL IDENTITY(1,1),
-	func_rol_rol_id smallint  not null UNIQUE, /* FK  THE_ULTIMATES.Rol*/
-	func_rol_func_id smallint not null UNIQUE /* FK  THE_ULTIMATES.Funcionalidad*/
+	func_rol_rol_id tinyint  not null, /* FK  THE_ULTIMATES.Rol*/
+	func_rol_func_id tinyint not null,/* FK  THE_ULTIMATES.Funcionalidad*/
+	CONSTRAINT PK_funcionalidad_rol PRIMARY KEY (func_rol_rol_id, func_rol_func_id)
 ); 
 
 GO
 
 create table THE_ULTIMATES.Usuario(
-	usu_id int PRIMARY KEY NOT NULL IDENTITY(1,1),
+	usu_id int CONSTRAINT PK_usu_id PRIMARY KEY NOT NULL IDENTITY(1,1),
 	usu_username varchar(20) not null UNIQUE,
 	usu_password char(64) not null,
 	usu_fecha_alta datetime not null,
@@ -43,15 +43,15 @@ create table THE_ULTIMATES.Usuario(
 	usu_respuesta char(64),
 	usu_activo bit DEFAULT 0,
 	usu_intentos_fallidos tinyint not null,
-	usu_clie_id int null /* FK  THE_ULTIMATES.CLiente*/
+	usu_clie_id int null /* FK  THE_ULTIMATES.Cliente*/
 );
 
 GO
 
 create table THE_ULTIMATES.Rol_Usuario(
-	rol_usu int PRIMARY KEY NOT NULL IDENTITY(1,1),
-	rol_usu_rol_id smallint  not null UNIQUE, /* FK  THE_ULTIMATES.Rol*/
-	rol_usu_usu_id smallint  not null UNIQUE  /* FK  THE_ULTIMATES.Usuario*/
+	rol_usu_rol_id tinyint  not null, /* FK  THE_ULTIMATES.Rol*/
+	rol_usu_usu_id int  not null,  /* FK  THE_ULTIMATES.Usuario*/
+	CONSTRAINT PK_rol_usuario PRIMARY KEY (rol_usu_rol_id, rol_usu_usu_id)
 );
 	
 GO
@@ -85,7 +85,7 @@ create table THE_ULTIMATES.Cliente(
 	clie_nombre varchar(50) not null,
 	clie_apellido varchar(50) not null,
 	clie_nro_doc int not null unique, 
-	clie_tipo_doc smallint not null, /* FK  THE_ULTIMATES.Tipo_Doc*/
+	clie_tipo_doc_id smallint not null, /* FK  THE_ULTIMATES.Tipo_Doc*/
 	clie_mail varchar(50) not null unique,
 	clie_activo bit not null,
 	clie_dom_calle varchar(50) not null,
@@ -93,7 +93,7 @@ create table THE_ULTIMATES.Cliente(
 	clie_dom_piso smallint null,
 	clie_dom_depto varchar(10),
 	clie_fecha_nac datetime not null,
-	clie_pais smallint not null /* FK  THE_ULTIMATES.Pais*/
+	clie_pais_id smallint not null /* FK  THE_ULTIMATES.Pais*/
 );
 
 GO
@@ -101,7 +101,7 @@ GO
 create table THE_ULTIMATES.Cuenta(
 	cuen_id int PRIMARY KEY not null IDENTITY(1,1),
 	cuen_clie_id int not null, /* FK  THE_ULTIMATES.Cliente*/
-	cuen_tipo_cuenta tinyint not null, /* FK  THE_ULTIMATES.Tipo_Cuenta*/
+	cuen_tipo_cuenta_id tinyint not null, /* FK  THE_ULTIMATES.Tipo_Cuenta*/
 	cuen_fecha_creacion datetime not null,
 	cuen_fecha_cierre datetime,
 	cuen_estado_id tinyint not null, /* FK  THE_ULTIMATES.Estado_Cuenta*/
@@ -151,7 +151,7 @@ create table THE_ULTIMATES.Tarjeta(
 GO
 
 create table THE_ULTIMATES.Emisor(
-	emisor_id int PRIMARY KEY not null IDENTITY(1,1),
+	emisor_id tinyint PRIMARY KEY not null IDENTITY(1,1),
 	emisor_desc varchar(30)
 ); 
 	
@@ -161,9 +161,9 @@ create table THE_ULTIMATES.Deposito(
 	depo_id int PRIMARY KEY NOT NULL IDENTITY(1,1),
 	depo_fecha datetime not null,
 	depo_importe numeric(18,2) not null,
-	depo_cuenta_id int not null,  /* FK  THE_ULTIMATES.Cuenta*/
+	depo_cuen_id int not null,  /* FK  THE_ULTIMATES.Cuenta*/
 	depo_tarj_id int not null,  /* FK  THE_ULTIMATES.Tarjeta*/
-	depo_tipo_moneda_id int not null /* FK  THE_ULTIMATES.Tipo_Moneda*/
+	depo_tipo_moneda_id smallint not null /* FK  THE_ULTIMATES.Tipo_Moneda*/
 );
 	
 GO
@@ -181,7 +181,7 @@ create table THE_ULTIMATES.Transferencia(
 	transf_id int PRIMARY KEY NOT NULL IDENTITY(1,1),
 	transf_fecha datetime not null,
 	transf_cuenta_origen int not null, /* FK  THE_ULTIMATES.Cuenta*/
-	transf_cueta_destino int not null,
+	transf_cuenta_destino int not null, /* FK  THE_ULTIMATES.Cuenta*/
 	transf_importe numeric(18,3)not null,
 	transf_costo_transf numeric(18,3)not null,
 	transf_cuenta_propia bit not null 
@@ -193,7 +193,7 @@ create table THE_ULTIMATES.Transaccion(
 	transac_id int PRIMARY KEY NOT NULL IDENTITY(1,1),
 	transac_fecha datetime not null,
 	transac_tipo_transac_id int not null, /* FK  THE_ULTIMATES.Cuenta*/
-	transac_imp_comision numeric(18,3)not null,
+	transac_importe_comision numeric(18,3)not null,
 	transac_pendiente bit not null	
 );
 
@@ -210,15 +210,15 @@ GO
 create table THE_ULTIMATES.Extraccion(
 	extrac_id int PRIMARY KEY NOT NULL IDENTITY(1,1),
 	extrac_cuenta_id int not null, /* FK  THE_ULTIMATES.Cuenta*/
-	extrac_cheq_id int not null /* FK  THE_ULTIMATES.Cheque*/
+	extrac_cheque_id int not null /* FK  THE_ULTIMATES.Cheque*/
 );
 
 GO
 
 create table THE_ULTIMATES.Banco(
-	banc_id int PRIMARY KEY NOT NULL IDENTITY(1,1),
-	banc_nombre varchar(50) not null,
-	banc_direc varchar(50) not null
+	banco_id int PRIMARY KEY NOT NULL IDENTITY(1,1),
+	banco_nombre varchar(50) not null,
+	banco_direc varchar(50) not null
 );
 
 GO
@@ -238,12 +238,110 @@ create table THE_ULTIMATES.Item_Factura(
 	item_fact_precio numeric(18,3)not null
 );
 
+
+/*create table THE_ULTIMATES.Item_Factura(
+	item_fact_num int not null, /* FK  THE_ULTIMATES.Factura*/
+	item_fact_transac_id int not null,  /* FK  THE_ULTIMATES.Transaccion*/
+	item_fact_cantidad int not null,
+	CONSTRAINT PK_item_factura PRIMARY KEY (item_fact_num, item_fact_transac_id)
+);
+*/
+
 GO
 
 /******************************************** FIN - CREACION DE TABLAS *********************************************/
 
 
 /******************************************** INICIO - FOREING KEY *************************************************/
+
+alter table THE_ULTIMATES.Funcionalidad_Rol
+add constraint FK_func_ron_rol_id foreign key (func_rol_rol_id) references THE_ULTIMATES.Rol(rol_id),
+	constraint FK_func_ron_func_id foreign key (func_rol_func_id) references THE_ULTIMATES.Funcionalidad(func_id);
+
+GO
+
+alter table THE_ULTIMATES.Usuario
+add constraint FK_usu_clie_id foreign key (usu_clie_id) references THE_ULTIMATES.Cliente(clie_id);
+
+go
+
+alter table THE_ULTIMATES.Rol_Usuario
+add constraint FK_rol_usu_rol_id foreign key (rol_usu_rol_id) references THE_ULTIMATES.Rol(rol_id),
+	constraint FK_rol_usu_usu_id foreign key (rol_usu_usu_id) references THE_ULTIMATES.Usuario(usu_id);
+	
+go
+
+alter table THE_ULTIMATES.Acceso_Log
+add constraint FK_acc_usu_id foreign key (acc_usu_id) references THE_ULTIMATES.Usuario(usu_id);
+
+go
+
+alter table THE_ULTIMATES.Cliente
+add constraint FK_clie_tipo_doc_id foreign key (clie_tipo_doc_id) references THE_ULTIMATES.Tipo_Doc(tipo_doc_id),
+	constraint FK_clie_pais_id foreign key (clie_pais_id) references THE_ULTIMATES.Pais(pais_id);
+
+go
+
+alter table THE_ULTIMATES.Cuenta
+add constraint FK_cuen_clie_id foreign key (cuen_clie_id) references THE_ULTIMATES.Cliente(clie_id),
+	constraint FK_cuen_tipo_cuenta_id foreign key (cuen_tipo_cuenta_id) references THE_ULTIMATES.Tipo_Cuenta(tipo_cuenta_id),
+	constraint FK_cuen_estado_id foreign key (cuen_estado_id) references THE_ULTIMATES.Estado_cuenta(esta_cuenta_id),
+	constraint FK_cuen_pais_id foreign key (cuen_pais_id) references THE_ULTIMATES.Pais(pais_id),
+	constraint FK_cuen_tipo_mon_id foreign key (cuen_tipo_mon_id) references THE_ULTIMATES.Tipo_Moneda(tipo_moneda_id);
+	
+go
+
+alter table THE_ULTIMATES.Tarjeta
+add constraint FK_tarj_clie_id foreign key (tarj_clie_id) references THE_ULTIMATES.Cliente(clie_id),
+	constraint FK_tarj_emisor_id foreign key (tarj_emisor_id) references THE_ULTIMATES.Emisor(emisor_id);
+	
+go
+
+alter table THE_ULTIMATES.Deposito
+add constraint FK_depo_cuen_id foreign key (depo_cuen_id) references THE_ULTIMATES.Cuenta(cuen_id),
+	constraint FK_depo_tarj_id foreign key (depo_tarj_id) references THE_ULTIMATES.Tarjeta(tarj_id),
+	constraint FK_depo_tipo_moneda_id foreign key (depo_tipo_moneda_id) references THE_ULTIMATES.Tipo_Moneda(tipo_moneda_id);
+
+go
+
+alter table THE_ULTIMATES.Cheque
+add constraint FK_cheque_banco_id foreign key (cheque_banco_id) references THE_ULTIMATES.Banco(banco_id);
+
+go
+
+alter table THE_ULTIMATES.Transferencia
+add constraint FK_transf_cuenta_origen foreign key (transf_cuenta_origen) references THE_ULTIMATES.Cuenta(cuen_id),
+	constraint FK_transf_cuenta_destino foreign key (transf_cuenta_destino) references THE_ULTIMATES.Cuenta(cuen_id);
+
+go
+
+alter table THE_ULTIMATES.Transaccion
+add constraint FK_transac_tipo_transac_id foreign key (transac_tipo_transac_id) references THE_ULTIMATES.Tipo_Transaccion(tipo_transac_id);
+
+go
+
+alter table THE_ULTIMATES.Extraccion
+add constraint FK_extrac_cuenta_id foreign key (extrac_cuenta_id) references THE_ULTIMATES.Cuenta(cuen_id),
+	constraint FK_extrac_cheque_id foreign key (extrac_cheque_id) references THE_ULTIMATES.Cheque(cheque_id);
+	
+go
+
+alter table THE_ULTIMATES.Factura
+add constraint FK_fact_clie_id foreign key (fact_clie_id) references THE_ULTIMATES.Cliente(clie_id);
+
+go
+
+alter table THE_ULTIMATES.Item_Factura
+add constraint FK_item_fact_num foreign key(item_fact_num) references THE_ULTIMATES.Factura(fact_num)/*,
+	constraint FK_item_fact_transac_id foreign key(item_fact_transac_id) references THE_ULTIMATES.Transaccion(transac_id)*/
+
+
+	
+
+
+
+
+
 /******************************************** FIN - FOREING KEY ****************************************************/
 
 
