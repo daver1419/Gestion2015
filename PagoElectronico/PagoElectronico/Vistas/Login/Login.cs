34ce312420a90cd6df21a33a37586e56e65ffe26
Emiliano <tenlion@hotmail.com>
2015-06-17 00:40:54 -0300

    Reorder de archivos en carpetas

---
+++ PagoElectronico/PagoElectronico/Vistas/Login/Login.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using PagoElectronico.Entidad;
+using PagoElectronico.PanelCliente;
+
+namespace PagoElectronico.Login
+{
+    public partial class Login : Form
+    {
+        Boolean posibleCliente;
+        Usuario usu;
+        PagoElectronico.ABM_de_Usuario.ControladorUsuario controladorUsuario = new PagoElectronico.ABM_de_Usuario.ControladorUsuario();
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            btnGuardar.Visible = true;
+            btnIngresar.Visible = false;
+            btnRegistrar.Visible = false;
+
+            posibleCliente = true;
+
+
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            if (textUsuario.Text.Trim().CompareTo("") == 0 || textContra.Text.Trim().CompareTo("") == 0)
+            {
+                MessageBox.Show("Debe ingresar Usuario y Contraseña", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            } else{
+              controladorUsuario.crearClientePosible(textUsuario.Text , textContra.Text, posibleCliente); 
+            } 
+        }
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+            if (textUsuario.Text.Trim().CompareTo("") == 0 || textContra.Text.Trim().CompareTo("") == 0)
+            {
+                MessageBox.Show("Debera ingresar un usuario y contraseña");
+
+            }
+
+            List<Usuario> listaU = new List<Usuario>();
+            listaU = controladorUsuario.buscarUsuario(textUsuario.Text, textContra.Text);
+
+            if (listaU.Count()== 0)
+            {  
+                MessageBox.Show("No se encontro Resultado para el usuario y contraseña ingresado ");
+            }else
+            if (listaU.First().habilitado )
+            {           
+                if (listaU.Count() == 2)
+                {
+                    lblrol.Visible = true;
+                    btnCliente.Visible = true;
+                    btnAdministrador.Visible = true;
+                    btnRegistrar.Visible = false;
+                    btnIngresar.Visible = false;
+                }
+                else
+                {
+
+
+                    if (listaU.First().rol == 1)
+                    {
+                        this.Hide();
+                        PagoElectronico.Panel.PanelAdmin panelAdmin = new PagoElectronico.Panel.PanelAdmin();
+                        panelAdmin.Show();
+                    }
+                    else if (listaU.First().rol == 2)
+                    {
+                        this.Hide();
+                        PagoElectronico.PanelCliente.PanelCliente panelCliente = new PagoElectronico.PanelCliente.PanelCliente();
+                        panelCliente.Show();
+                    }
+                }
+
+
+            }else
+            {
+                  MessageBox.Show("El usuario se encuentra bloqueado");
+               
+            }
+        }
+
+        private void btnCliente_Click(object sender, EventArgs e)
+        {
+            this.Close();
+
+            PagoElectronico.PanelCliente.PanelCliente panelCliente = new PagoElectronico.PanelCliente.PanelCliente();
+        
+        }
+
+        private void btnAdministrador_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            PagoElectronico.Panel.PanelAdmin panelAdm = new PagoElectronico.Panel.PanelAdmin();
+
+        
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            textUsuario.Text = " ";
+            textContra.Text = " ";
+            btnGuardar.Visible = false;
+            btnCancelar.Visible = false;
+            lblrol.Visible = false;
+            btnCliente.Visible = false;
+            btnAdministrador.Visible = false;
+            btnIngresar.Visible = true;
+            btnRegistrar.Visible = true;
+
+        }
+
+        private void textUsuario_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+      
+       
+
+      
+
+
+
+
+
+
+
+      
+    }
+}
---
+++ PagoElectronico/PagoElectronico/Vistas/Panel/PanelAdmin.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using PagoElectronico.Entidad;
+using PagoElectronico.DAO;
+
+
+namespace PagoElectronico.Panel
+{
+    public partial class PanelAdmin : Form
+    {
+     
+
+       private RolDAO rolDAO;
+
+       private SisDAO sisDAO;
+       private CuentaDao cuentaDao;
+       private string _cliente;
+        
+        Rol rolSelect = new Rol();
+
+
+        public PanelAdmin()
+        {
+
+            
+            InitializeComponent();
+            
+         
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            loadCombo();
+           
+        }
+
+        private void btnAltaClienteUsu_Click(object sender, EventArgs e)
+        {
+            tab.SelectedTab = tabCliente;
+            panelBusquedaUsu.Visible = false;
+
+        }
+
+        private void btnAddTarjeta_Click(object sender, EventArgs e)
+        {
+            tab.SelectedTab = tabTarjeta;
+            // llenar los label para cliente y  numero de cuenta 
+
+            lblNcuentaTar.Text = txtNumeroCuenta.Text;
+           
+
+
+        }
+
+        private void btnConfTarj_Click(object sender, EventArgs e)
+        {
+            btnAddTarjeta.Visible=true;
+        }
+
+        private void loadCombo()
+        {
+            rolDAO = new RolDAO();
+            sisDAO = new SisDAO();
+            // panel usuario
+            List<Rol> dt = rolDAO.listaRol();
+            cbxRol.DataSource = dt;
+            cbxRol.DisplayMember = "DESCRIPCION";
+            cbxRol.Text = "Elegir una";
+
+            //panel Cliente
+            List<Pais> paises = sisDAO.listaPais();
+            cbxCliPais.DataSource = paises;
+            cbxCliPais.DisplayMember = "DESCRIPCION";
+            cbxCliPais.Text = "Elegir una";
+
+
+
+            /*
+            List<TipoDoc> tiposDoc = sisDAO.listaTipoDoc();
+            cbxCliTipoDoc.DataSource = tiposDoc;
+            cbxCliTipoDoc.DisplayMember = "DESCRIPCION";
+            cbxCliTipoDoc.Text = "Elegir una";
+
+            // panel Cuenta
+            cbxPaisCuenta.DataSource = paises;
+            cbxPaisCuenta.DisplayMember = "DESCRIPCION";
+            cbxPaisCuenta.Text = "Elegir una";
+
+            List<TipoMoneda> tiposMonedas = sisDAO.listaTipoMoneda();
+            cbxMoneda.DataSource = tiposMonedas;
+            cbxMoneda.DisplayMember = "DESCRIPCION";
+            cbxMoneda.Text = "Elegir una";
+
+            List<TipoCuenta> tiposCuentas = sisDAO.listaTipoCuenta();
+            cbxTipoCuenta.DataSource = tiposCuentas;
+            cbxTipoCuenta.DisplayMember = "DESCRIPCION";
+            cbxTipoCuenta.Text = "Elegir una";
+           
+            // tab Tarjeta Falta el combo box 
+
+            // tab Saldo 
+            cbxTipoDocSaldo.DataSource = tiposDoc;
+            cbxTipoDocSaldo.DisplayMember = "DESCRIPCION";
+            cbxTipoDocSaldo.Text = "Elegir una";   */
+        }
+
+        private void cbxRol_SelectedValueChanged(object sender, EventArgs e)
+        {         
+
+
+                    
+        }
+
+        private void btnFunRol1_Click(object sender, EventArgs e)
+        {
+            rolSelect = (Rol)cbxRol.SelectedValue;
+
+            rolDAO = new RolDAO();
+
+            List<Funcionalidad> funcionalidades = new List<Funcionalidad>();
+
+            funcionalidades = rolDAO.listaFuncionalidad(rolSelect.id);
+
+            lisboxFuncionalidades.DataSource = funcionalidades;
+            lisboxFuncionalidades.DisplayMember = "DESCRIPCION";
+
+
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            cuentaDao = new CuentaDao();
+            List<Cuenta> cuentas = cuentaDao.listaCuentas(Convert.ToInt32(_cliente));
+            checkedListBox2.DataSource = cuentas;
+            checkedListBox2.DisplayMember = "Cuentas";
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+            TextBox clienteTextBox = (TextBox)sender;
+            _cliente = clienteTextBox.Text;
+
+        }
+
+        private void checkedListBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+      
+        
+    }
+}
+        
+     
+      
+
+        
+       
+      
+
+      
+
+     
+
+      
+
+        
+
+       
