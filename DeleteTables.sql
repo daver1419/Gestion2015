127bd5342b40b6e6abb4fad1bb0eef9474bab2cc
David <daver1419@gmail.com>
2015-06-26 02:28:36 -0300

    fix: drop alter table constraint

--- DeleteTables.sql
+++ DeleteTables.sql
@@ -22,7 +22,7 @@ alter table THE_ULTIMATES.Deposito drop constraint FK_depo_tipo_moneda_id;
 alter table THE_ULTIMATES.Cheque drop constraint FK_cheque_banco_id;
 alter table THE_ULTIMATES.Transferencia drop constraint FK_transf_cuenta_origen;
 alter table THE_ULTIMATES.Transferencia drop constraint FK_transf_cuenta_destino;
-alter table THE_ULTIMATES.Tannsferencia drop constraint FK_transf_transac_id;
+alter table THE_ULTIMATES.Transferencia drop constraint FK_transf_transac_id;
 alter table THE_ULTIMATES.Transaccion drop constraint FK_transac_tipo_transac_id;
 alter table THE_ULTIMATES.Transaccion drop constraint FK_transac_cuen_id;
 alter table THE_ULTIMATES.Extraccion drop constraint FK_extrac_cuenta_id;
